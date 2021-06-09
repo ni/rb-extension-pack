c4446490bb60e3d35014b7c6ff4152773e439903
KMJaimes <39599684+kmjaimes@users.noreply.github.com>
2021-06-09 12:21:25 -0500

    Update setup.py
    Scot Salmon no longer at company

--- rb_p4_email_followers/setup.py
+++ rb_p4_email_followers/setup.py
@@ -8,10 +8,10 @@ setup(
     name="rb_p4_email_followers",
     version="0.1.1",
     description="Extension for Review Board to email p4 reviews when a review is posted",
-    author="Scot Salmon",
-    author_email="scot.salmon@ni.com",
-    maintainer="Scot Salmon",
-    maintainer_email="scot.salmon@ni.com",
+    author="Kristen Jaimes",
+    author_email="kristen.jaimes@ni.com",
+    maintainer="Dawn Thomas",
+    maintainer_email="dawn.thomas@ni.com",
     packages=find_packages(),
     entry_points={
         "reviewboard.extensions": [
